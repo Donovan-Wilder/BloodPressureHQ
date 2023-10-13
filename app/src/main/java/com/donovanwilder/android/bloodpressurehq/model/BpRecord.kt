c42f21e0aebfb10df68d07806ffbcbe3594fdd26
Donovan Wilder <donovan.wilder@gmail.com>
2023-10-12 22:51:26 -0400

    change: hardcode table name

--- app/src/main/java/com/donovanwilder/android/bloodpressurehq/model/BpRecord.kt
+++ app/src/main/java/com/donovanwilder/android/bloodpressurehq/model/BpRecord.kt
@@ -7,29 +7,11 @@ import androidx.room.PrimaryKey
 import java.util.Date
 
 
-@Entity(tableName = BpRecord.TABLE_NAME)
+@Entity(tableName = "bp_record")
 data class BpRecord(
     @PrimaryKey(true) @ColumnInfo(name = "_id") val id: Int = 0,
     @ColumnInfo(name = "date_added") val dateAdded: Date,
     @ColumnInfo(name = "sys") val sys: Int,
     @ColumnInfo(name = "dia") val dia: Int,
     @ColumnInfo(name = "pulse") val pulse: Int
-) {
-    override fun equals(other: Any?): Boolean {
-        val otherRecord = other as BpRecord
-        return (otherRecord.id == this.id) &&
-                (otherRecord.dateAdded == this.dateAdded) &&
-                (otherRecord.sys == this.sys) &&
-                (otherRecord.dia == this.dia) &&
-                (otherRecord.pulse == this.pulse)
-    }
-
-    companion object {
-        const val TABLE_NAME = "bp_record"
-        const val sysMaxValue = 400
-        const val sysMinValue = 50
-        const val diaMaxValue = 400
-        const val diaMinValue = 20
-        const val pulseMaxValue = 500
-    }
-}
+)
