130791f90f1d91d80d74b73eaa313f7d6a06b793
Donovan Wilder <donovan.wilder@gmail.com>
2023-10-12 22:51:38 -0400

    change: hardcode table name

--- app/src/main/java/com/donovanwilder/android/bloodpressurehq/database/BpRecordDao.kt
+++ app/src/main/java/com/donovanwilder/android/bloodpressurehq/database/BpRecordDao.kt
@@ -13,15 +13,15 @@ interface BpRecordDao {
     @Insert
     suspend fun insertAll(vararg bpRecord: BpRecord)
 
-    @Query("SELECT * FROM ${BpRecord.TABLE_NAME} ORDER BY date_added ASC")
+    @Query("SELECT * FROM bp_record ORDER BY date_added ASC")
     fun getAll(): Flow<List<BpRecord>>
 
-    @Query("UPDATE ${BpRecord.TABLE_NAME} SET sys = :sysValue, dia = :diaValue, pulse = :pulseValue, date_added = :dateAdded WHERE _id = :id")
+    @Query("UPDATE bp_record SET sys = :sysValue, dia = :diaValue, pulse = :pulseValue, date_added = :dateAdded WHERE _id = :id")
     suspend fun update(id: Int, sysValue: Int, diaValue: Int, pulseValue: Int, dateAdded: Date)
 
-    @Query("SELECT MIN(date_added) FROM ${BpRecord.TABLE_NAME}")
+    @Query("SELECT MIN(date_added) FROM bp_record")
     suspend fun getEarliestDate(): Date
-    @Query("SELECT 0 AS _id, :fromDate AS date_added, AVG(sys) AS sys, AVG(dia) AS dia, AVG(pulse) AS pulse FROM ${BpRecord.TABLE_NAME} WHERE date_added BETWEEN :fromDate  AND :toDate")
+    @Query("SELECT 0 AS _id, :fromDate AS date_added, AVG(sys) AS sys, AVG(dia) AS dia, AVG(pulse) AS pulse FROM bp_record WHERE date_added BETWEEN :fromDate  AND :toDate")
     suspend fun getAvgFromDateRange(fromDate:Date,toDate:Date):BpRecord
     @Delete
     suspend fun delete(bpRecord: BpRecord) }
