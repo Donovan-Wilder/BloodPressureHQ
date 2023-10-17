495aaa393353664b841b583a649785c2ed9d026c
Donovan Wilder <donovan.wilder@gmail.com>
2023-10-17 12:21:09 -0400

    change: delete unused metods

--- app/src/main/java/com/donovanwilder/android/bloodpressurehq/BpRecordRepository.kt
+++ app/src/main/java/com/donovanwilder/android/bloodpressurehq/BpRecordRepository.kt
@@ -10,10 +10,8 @@ class BpRecordRepository private constructor(private val context: Context){
     private val database = Room.databaseBuilder(this.context,BpRecordDatabase::class.java,
         BpRecordDatabase.DATABASE_NAME).build()
 
-    suspend fun getAvgFromDateRange(fromDate:Date, toDate:Date) = database.bpRecordDao().getAvgFromDateRange( fromDate,toDate)
     suspend fun addRecord(vararg bpRecord: BpRecord) = database.bpRecordDao().insertAll(*bpRecord)
     fun getAllRecords()= database.bpRecordDao().getAll()
-    suspend fun getEarliestDate() = database.bpRecordDao().getEarliestDate()
     suspend fun updateRecord(id:Int, sysValue: Int, diaValue:Int, pulseValue:Int,dateAdded:Date) = database.bpRecordDao().update(id,sysValue,diaValue,pulseValue,dateAdded)
     suspend fun deleteRecord(bpRecord: BpRecord)=database.bpRecordDao().delete(bpRecord)
     companion object{
--- app/src/main/java/com/donovanwilder/android/bloodpressurehq/database/BpRecordDao.kt
+++ app/src/main/java/com/donovanwilder/android/bloodpressurehq/database/BpRecordDao.kt
@@ -19,9 +19,5 @@ interface BpRecordDao {
     @Query("UPDATE bp_record SET sys = :sysValue, dia = :diaValue, pulse = :pulseValue, date_added = :dateAdded WHERE _id = :id")
     suspend fun update(id: Int, sysValue: Int, diaValue: Int, pulseValue: Int, dateAdded: Date)
 
-    @Query("SELECT MIN(date_added) FROM bp_record")
-    suspend fun getEarliestDate(): Date
-    @Query("SELECT 0 AS _id, :fromDate AS date_added, AVG(sys) AS sys, AVG(dia) AS dia, AVG(pulse) AS pulse FROM bp_record WHERE date_added BETWEEN :fromDate  AND :toDate")
-    suspend fun getAvgFromDateRange(fromDate:Date,toDate:Date):BpRecord
     @Delete
     suspend fun delete(bpRecord: BpRecord) }
