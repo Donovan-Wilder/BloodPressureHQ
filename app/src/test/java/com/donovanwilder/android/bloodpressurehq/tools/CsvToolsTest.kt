b729c88236e678865d34f575a41da494cfc1df8d
Donovan Wilder <donovan.wilder@gmail.com>
2023-10-28 09:46:42 -0400

    test: add tests that check for the correct number of records and the correct values when creating CsvStrings

--- app/src/test/java/com/donovanwilder/android/bloodpressurehq/tools/CsvToolsTest.kt
+++ app/src/test/java/com/donovanwilder/android/bloodpressurehq/tools/CsvToolsTest.kt
@@ -1,7 +1,11 @@
 package com.donovanwilder.android.bloodpressurehq.tools
 
+import com.donovanwilder.android.bloodpressurehq.model.BpRecord
 import org.junit.Assert.*
 import org.junit.Test
+import java.util.Date
+import java.util.Scanner
+import java.util.regex.Pattern
 
 class CsvToolsTest {
     @Test
@@ -31,7 +35,7 @@ class CsvToolsTest {
             """.trimIndent()
         val result = CsvTools.createBpRecordList(csvString)
 
-        var ( dateAdded, sys, dia, pulse) = arrayOf(100, 120, 70, 60)
+        var (dateAdded, sys, dia, pulse) = arrayOf(100, 120, 70, 60)
         for (record in result) {
 
             assertEquals(dateAdded.toLong(), record.dateAdded.time)
@@ -43,5 +47,68 @@ class CsvToolsTest {
             dia += 5
             pulse += 5
         }
+    }
+
+    @Test
+    fun Should_CreateTheRightNumberOfLines_When_CreatingACsvString() {
+        val recordData = listOf(
+            BpRecord(0, Date(500), 120, 85, 60),
+            BpRecord(0, Date(400), 115, 80, 55),
+            BpRecord(0, Date(300), 110, 75, 50)
+        )
+        val result = CsvTools.createCsvString(recordData)
+        val scanner = Scanner(result)
+
+        var lineCounter = -1 // Subtract one for the column names line
+        while (scanner.hasNextLine()) {
+            scanner.nextLine()
+            lineCounter++
+        }
+
+        assertEquals(recordData.size, lineCounter)
+    }
+
+    @Test
+    fun Should_CreateTheRightValuesInRecord_When_CreatingACsvString() {
+
+        val recordData = listOf(
+            BpRecord(0, Date(500), 120, 85, 60),
+            BpRecord(0, Date(400), 115, 80, 55),
+            BpRecord(0, Date(300), 110, 75, 50)
+        )
+        val result = CsvTools.createCsvString(recordData)
+        val scanner = Scanner(result)
+
+        val parsingRegex = "^(\\d+),\\s?(\\d+),\\s?(\\d+),\\s?(\\d+),\\s?(\\d+)"
+        val pattern = Pattern.compile(parsingRegex)
+
+
+        scanner.nextLine()
+        var index = 0
+        while (scanner.hasNextLine()) {
+            val line = scanner.nextLine()
+            val matcher = pattern.matcher(line)
+            println("index: $index\nrecord: ${recordData[index]}\nline: $line\nmatcher: $matcher.")
+            if (matcher.find()) {
+                assertEquals(
+                    recordData[index].dateAdded.time,
+                    matcher.group(2)?.toLong() ?: throw Exception("No group value")
+                )
+                assertEquals(
+                    recordData[index].sys,
+                    matcher.group(3)?.toInt() ?: throw Exception("No group value")
+                )
+                assertEquals(
+                    recordData[index].dia,
+                    matcher.group(4)?.toInt() ?: throw Exception("No group value")
+                )
+                assertEquals(
+                    recordData[index].pulse,
+                    matcher.group(5)?.toInt() ?: throw Exception("No group value")
+                )
+                index++
+            }
+        }
+
     }
 }
