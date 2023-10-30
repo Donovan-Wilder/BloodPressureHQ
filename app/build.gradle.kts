e60cc59266a26a694c335c6b499e4c61056adfda
Donovan Wilder <donovan.wilder@gmail.com>
2023-10-30 11:30:32 -0400

    build: add dependencies for testing composables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,11 +55,11 @@ android {
 }
 
 dependencies {
-
+    val compose_version = "1.5.1"
     testImplementation("io.mockk:mockk:1.9.3")
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
-    implementation("androidx.compose.ui:ui-android:1.5.1")
+    implementation("androidx.compose.ui:ui-android:$compose_version")
     //Navigation
     val nav_version = "2.7.3"
 
@@ -97,6 +97,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$compose_version")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
